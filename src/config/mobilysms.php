8d91d7f1b54b80b52fff92763492fc1ab8ff007d
ahmed-aliraqi <aliraqi.dev@gmail.com>
2017-05-09 11:41:46 +0200

    first commit

---
+++ src/config/mobilysms.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'sender'  =>'Ahmed', // Sender Name With Mobily.ws
+	'mobile'  =>'123456789', // Mobile User Account With Mobily.ws
+	'password'=>'password here', // Password Account With Mobile.ws
+	'timeSend'=>0, // If You Don't know what this please don't change anything
+	'dateSend'=>0, // If You Don't know what this please don't change anything
+	'deleteKey'=>152485, // If You Don't know what this please don't change anything
+	'resultType'=>1, // If You Don't know what this please don't change anything
+	'viewResult'=>1, // If You Don't know what this please don't change anything
+	'MsgID'=>rand(0000,9999), // If You Don't know what this please don't change anything
+];
